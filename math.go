79ddaca87daf3607cd40d8738baaeb4c8bea3bb4
Heliandro Lavor <heliandro.l@gmail.com>
2022-09-12 14:06:01 -0300

    feat: add novas funcoes para calcular

--- math.go
+++ math.go
@@ -9,3 +9,15 @@ func main() {
 func soma(a int, b int) int {
 	return a + b
 }
+
+func sub(a int, b int) int {
+	return a - b
+}
+
+func multi(a int, b int) int {
+	return a * b
+}
+
+func divi(a int, b int) int {
+	return a / b
+}
