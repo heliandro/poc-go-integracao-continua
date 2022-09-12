426b6dd51b89b245cc26c2b75085876a63bfbbdd
Heliandro Lavor <heliandro.l@gmail.com>
2022-09-12 14:32:32 -0300

    test: add math coverage para func sub

--- math_test.go
+++ math_test.go
@@ -10,3 +10,12 @@ func TestSoma(t *testing.T) {
 	}
 	
 }
+
+func TestSub(t *testing.T) {
+	total := sub(10, 7);
+
+	if total != 3 {
+		t.Errorf("Resultado da sub é inválido. Resultado: %d Esperado: %d", total, 3)
+	}
+	
+}
